c29e42d91af58289211b06058f023106d6ef8485
Jamie Zeng <jzeng3@stuy.edu>
2019-04-01 15:57:42 -0400

    radixsort linked lists work for ones digit

--- Radix.java
+++ Radix.java
@@ -1,21 +1,30 @@
+import java.util.*;
+
 public class Radix{
   public static void radixsort(int[]data){
     radixsort(data, 10);
   }
-
+  @SuppressWarnings("unchecked")
   public static void radixsort(int[] data, int digit){
     @SuppressWarnings("unchecked")
-    MyLinkedList<Integer>[] buckets = (MyLinkedList<Integer>[])new Object[20];
+    List< MyLinkedList<Integer> > buckets = new ArrayList<>(20);
 
-    for (int i = 0; i < buckets.length; i++){
-      buckets[i] = new MyLinkedList<Integer>();
+    for (int i = 0; i < 20; i++){
+      buckets.add( new MyLinkedList<Integer>() );
     }
+    System.out.println("bucket size:"+buckets.size());
+
     for (int i = 0; i < data.length; i++){
       if (data[i]%digit > 0){
-        buckets[10 + data[i]%digit].add(data[i]%digit);
-        System.out.println(buckets[10 + data[i]%digit]);
+        System.out.println(data[i]%digit);
+        buckets.get(10 + data[i]%digit).add(data[i]);
+      System.out.println(buckets.get( 10 + data[i]%digit));
+      }
+      else{
+        System.out.println(data[i]%digit);
       }
     }
+
 
 
   }
