10e271b55ec0159826dd503683298f628f3a17f6
Jamie Zeng <jzeng3@stuy.edu>
2019-04-01 10:10:20 -0400

    started adding elements to linked lists

--- Radix.java
+++ Radix.java
@@ -1,10 +1,22 @@
 public class Radix{
-  public void radixsort(int[]data){
-    MyLinkedList<Integer> bucketList = new MyLinkedList<Integer>();
-    radixsort(data, 10, bucketList);
+  public static void radixsort(int[]data){
+    radixsort(data, 10);
   }
 
-  public void radixsort(int[] data, int digit, MyLinkedList<Integer> bucketList){
+  public static void radixsort(int[] data, int digit){
+    @SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] buckets = (MyLinkedList<Integer>[])new Object[20];
+
+    for (int i = 0; i < buckets.length; i++){
+      buckets[i] = new MyLinkedList<Integer>();
+    }
+    for (int i = 0; i < data.length; i++){
+      if (data[i]%digit > 0){
+        buckets[10 + data[i]%digit].add(data[i]%digit);
+        System.out.println(buckets[10 + data[i]%digit]);
+      }
+    }
+
 
   }
 }
