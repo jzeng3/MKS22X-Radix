a3d61ae769f4ee5e190b704f1dacb6b430f85f70
Jamie Zeng <jzeng3@stuy.edu>
2019-04-02 15:15:58 -0400

    added digit counter and loop to radixsort

--- Radix.java
+++ Radix.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class Radix{
   public static void radixsort(int[]data){
-    radixsort(data, 10);
+    radixsort(data, 1);
   }
 
   public static void radixsort(int[] data, int digit){
     @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    maxDigits(data);
+    int maxDigits = 1;
+
     for (int i = 0; i < 20; i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
@@ -16,15 +17,18 @@ public class Radix{
     System.out.println("bucket size: "+buckets.length);
 
     for (int i = 0; i < data.length; i++){
-      if (data[i]%digit > 0){
-        System.out.println(data[i]%digit);
-        buckets[10 + data[i]%digit].addEnd( data[i] );
-        System.out.println(buckets[ 10 + data[i]%digit ]);
+      if (String.valueOf( Math.abs(data[i]) ).length() > maxDigits){
+        maxDigits = String.valueOf( Math.abs(data[i]) ).length();
+      }
+      if (data[i] >= 0){
+        System.out.println(data[i]%10);
+        buckets[10 + data[i]%10].addEnd( data[i] );
+        System.out.println(buckets[ 10 + data[i]%10 ]);
       }
       else{
-        System.out.println(data[i]%digit);
-        buckets[9 + data[i]%digit].addFront(data[i]);
-        System.out.println(buckets[9 - data[i]%digit]);
+        System.out.println(data[i]%10);
+        buckets[9 + data[i]%10].addFront(data[i]);
+        System.out.println(buckets[9 + data[i]%10]);
       }
     }
     // new merged linked list
@@ -35,19 +39,36 @@ public class Radix{
         System.out.println( buckets[i] );
       }
     }
+    maxDigits--;
+    digit *= 10;
+    while (maxDigits > 0){
+      System.out.println("digit: "+digit);
+      while (merged.size() > 0){
+        int removed = merged.removeFront();
+        System.out.println("removed element: "+removed);
+        if (removed >= 0){
+          System.out.println((removed / digit) % 10);
+          buckets[10 + ( (removed / digit) % 10)].addEnd( removed );
+          System.out.println(buckets[ 10 +((removed / digit) % 10)]);
+        }
+        else{
+          System.out.println( (removed / digit) % 10);
+          buckets[9 + ( (removed / digit) % 10)].addFront(removed);
+          System.out.println(buckets[9 + ( (removed / 10) % 10)]);
+        }
 
-  }
-
-  public static int maxDigits(int[] data){
-    int[] copy = new int[data.length];
-    for (int i = 0; i < data.length; i++){
-      copy[i] = data[i];
+      }
+      System.out.println("merged, should be empty: "+merged);
+      for (int i = 0; i < 20; i++){
+        if (buckets[i].size() > 0){
+          merged.extend( buckets[i] );
+          System.out.println( buckets[i] );
+        }
+      }
+      System.out.println("merged, should be sorted by digit: "+merged);
+      maxDigits--;
+      digit *= 10;
     }
-    int max = 0;
-    Arrays.sort(copy);
-    max = Math.max( Math.abs(copy[0]), Math.abs(copy[copy.length - 1]));
-    int digits = String.valueOf( max ).length();
-    System.out.println("digits: "+digits);
-    return digits;
   }
+
 }
