a4800a723137648cbda0a9d7030d463d9c3d78ec
Jamie Zeng <jzeng3@stuy.edu>
2019-04-02 09:42:15 -0400

    array of buckets works

--- Radix.java
+++ Radix.java
@@ -4,28 +4,34 @@ public class Radix{
   public static void radixsort(int[]data){
     radixsort(data, 10);
   }
-  @SuppressWarnings("unchecked")
+
   public static void radixsort(int[] data, int digit){
     @SuppressWarnings("unchecked")
-    List< MyLinkedList<Integer> > buckets = new ArrayList<>(20);
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
 
     for (int i = 0; i < 20; i++){
-      buckets.add( new MyLinkedList<Integer>() );
+      buckets[i] = new MyLinkedList<Integer>();
     }
-    System.out.println("bucket size:"+buckets.size());
+    System.out.println("bucket size:"+buckets.length);
 
     for (int i = 0; i < data.length; i++){
       if (data[i]%digit > 0){
         System.out.println(data[i]%digit);
-        buckets.get(10 + data[i]%digit).add(data[i]);
-      System.out.println(buckets.get( 10 + data[i]%digit));
+        buckets[10 + data[i]%digit].add(data[i]);
+        System.out.println(buckets[ 10 + data[i]%digit ]);
       }
       else{
         System.out.println(data[i]%digit);
+        buckets[9 - data[i]%digit].add(data[i]);
+        System.out.println(buckets[9 - data[i]%digit]);
       }
     }
-
-
-
+    MyLinkedList<Integer> merged = new MyLinkedList<Integer>();
+    for (int i = 0; i < 20; i++){
+      if (buckets[i].size() > 0){
+        merged.extend( buckets[i] );
+      }
+    }
+    System.out.println( merged );
   }
 }
