b56dacb48b059cf414b592b56a18c1fec55ce9d7
Jamie Zeng <jzeng3@stuy.edu>
2019-04-02 10:11:01 -0400

    wrote maxDigits helper method to find how many times to loop through data

--- Radix.java
+++ Radix.java
@@ -8,7 +8,7 @@ public class Radix{
   public static void radixsort(int[] data, int digit){
     @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-
+    maxDigits(data);
     for (int i = 0; i < 20; i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
@@ -27,6 +27,7 @@ public class Radix{
         System.out.println(buckets[9 - data[i]%digit]);
       }
     }
+    // new merged linked list
     MyLinkedList<Integer> merged = new MyLinkedList<Integer>();
     for (int i = 0; i < 20; i++){
       if (buckets[i].size() > 0){
@@ -34,6 +35,19 @@ public class Radix{
         System.out.println( buckets[i] );
       }
     }
-    System.out.println( merged );
+
+  }
+
+  public static int maxDigits(int[] data){
+    int[] copy = new int[data.length];
+    for (int i = 0; i < data.length; i++){
+      copy[i] = data[i];
+    }
+    int max = 0;
+    Arrays.sort(copy);
+    max = Math.max( Math.abs(copy[0]), Math.abs(copy[copy.length - 1]));
+    int digits = String.valueOf( max ).length();
+    System.out.println("digits: "+digits);
+    return digits;
   }
 }
