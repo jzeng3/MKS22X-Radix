71a480b5ff69123114a553720cfe55302ea17006
Jamie Zeng <jzeng3@stuy.edu>
2019-04-02 21:00:57 -0400

    addFront should only be used once?

--- Radix.java
+++ Radix.java
@@ -6,29 +6,30 @@ public class Radix{
   }
 
   public static void radixsort(int[] data, int digit){
+    // initialize array of linked lists to hold 20 digits (-9 to 9, inclusive)
     @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    int maxDigits = 1;
-
     for (int i = 0; i < 20; i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
 
-  //  System.out.println("bucket size: "+buckets.length);
+    // number of digits in the largest number of the array
+    int maxDigits = 1;
 
     for (int i = 0; i < data.length; i++){
       if (String.valueOf( Math.abs(data[i]) ).length() > maxDigits){
         maxDigits = String.valueOf( Math.abs(data[i]) ).length();
       }
       if (data[i] >= 0){
-      //  System.out.println(data[i]%10);
+        System.out.println("This element is non-negative");
         buckets[10 + data[i]%10].addEnd( data[i] );
-      //  System.out.println(buckets[ 10 + data[i]%10 ]);
+        System.out.println(buckets[ 10 + data[i]%10 ]);
       }
       else{
+          System.out.println("This element is negative");
     //    System.out.println(data[i]%10);
         buckets[9 + data[i]%10].addFront(data[i]);
-    //    System.out.println(buckets[9 + data[i]%10]);
+        System.out.println(buckets[9 + data[i]%10]);
       }
     }
     // new merged linked list
@@ -39,6 +40,7 @@ public class Radix{
       //  System.out.println( buckets[i] );
       }
     }
+    System.out.println("Merged, with digit 1: "+merged);
     maxDigits--;
     digit *= 10;
     while (maxDigits > 0){
@@ -47,20 +49,27 @@ public class Radix{
         int removed = merged.removeFront();
     //    System.out.println("removed element: "+removed);
         if (removed >= 0){
+          System.out.println("This element is non-negative");
         //  System.out.println((removed / digit) % 10);
           buckets[10 + ( (removed / digit) % 10)].addEnd( removed );
       //    System.out.println(buckets[ 10 +((removed / digit) % 10)]);
         }
         else{
+          int index = 9 + ( (removed / digit) % 10);
+              System.out.println("This element is negative");
+
+
         //  System.out.println( (removed / digit) % 10);
-          buckets[9 + ( (removed / digit) % 10)].addFront(removed);
+          buckets[9 + ( (removed / digit) % 10)].addEnd(removed);
         //  System.out.println(buckets[9 + ( (removed / 10) % 10)]);
+          System.out.println("bucket "+index+": "+buckets[index]);
         }
 
       }
 
       for (int i = 0; i < 20; i++){
         if (buckets[i].size() > 0){
+          System.out.println("bucket "+i+": "+buckets[i]);
           merged.extend( buckets[i] );
       //    System.out.println( buckets[i] );
         }
